6af794506ff8cce12bb497bb3e1f925b2398643d
sebasgonzac27 <sebasgonzac27@gmail.com>
2023-01-05 19:03:24 -0500

    file for proofs

---
+++ src/prueba.js
@@ -0,0 +1,7 @@
+import { originalMails } from "../src/mails.js";
+
+console.log("Estos son los mails originales:");
+console.log(originalMails);
+
+console.log("Estos son los mails en orden inverso");
+console.log(originalMails.reverse());
